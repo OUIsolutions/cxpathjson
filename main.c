ebc5226d2a226e94f6e40f551e7103b32d87224b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-25 07:09:51 -0300

    at

--- main.c
+++ main.c
@@ -5,9 +5,11 @@ int  main(){
     
     int error = 0;
     cJSON *t = cjson_path_load_from_file(&error,"teste.json");
-    int result = cjson_path_set_str(t,"aaaaaaa","['a','$append','c']");
-    printf("result: %d\n",result);
+    error  = cjson_path_set_str(t,"aaaaaaa","['a',-1,'c']");
+    printf("result: %d\n",error);
+
     cjson_path_dump_to_file("a.json",t,true);
+
     cjson_path_free(t);
 
 
