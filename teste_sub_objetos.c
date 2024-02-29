79e1932756c19fe604ecb0647a91db56c571c496
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-29 18:25:14 -0300

    at

--- teste_sub_objetos.c
+++ teste_sub_objetos.c
@@ -10,29 +10,29 @@ CxpathJsonErrorNamespace errors;
 int main(){
     xpath = newCxpathJsonNamespace();
     errors = xpath.errors;
-    CxpathJson *t = xpath.new_from_file("a.json");
-    CxpathJson *a = xpath.get_object(t,"['a']");
+    CxpathJson *rooot = xpath.new_from_file("a.json");
+    CxpathJson *a = xpath.get_object(rooot, "['a']");
     char *a1 = xpath.get_str(a,"['a1']");
-    if(!errors.has_errors(t)){
+    if(!errors.has_errors(rooot)){
         printf("a1:%s\n",a1);
     }
 
 
-    CxpathJson *b = xpath.get_array(t,"['b']");
+    CxpathJson *b = xpath.get_array(rooot, "['b']");
     char  *b0 =xpath.get_str(b,"[0]");
-    if(!errors.has_errors(t)){
+    if(!errors.has_errors(rooot)){
         printf("b0: %s\n",b0);
     }
 
-    if(errors.has_errors(t)){
-        char *message =errors.get_error_message(t);
-        int code = errors.get_error_code(t);
+    if(errors.has_errors(rooot)){
+        char *message =errors.get_error_message(rooot);
+        int code = errors.get_error_code(rooot);
         printf("%d",code);
         printf("%s",message);
-        xpath.free(t);
+        xpath.free(rooot);
         return 0;
     }
 
-    xpath.free(t);
+    xpath.free(rooot);
 
 }
