086b3f5d2052dc6c46989c7ae124686973d386ae
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-01 18:49:11 -0300

    att

---
+++ exemples/extra/delete.c
@@ -0,0 +1,29 @@
+
+#include "CxpathJson.h"
+
+
+
+CxpathJsonNamespace xpath;
+CxpathJsonErrorNamespace errors;
+
+
+int main(){
+    xpath = newCxpathJsonNamespace();
+    errors = xpath.errors;
+    CxpathJson *t = xpath.new_from_file("tests/target/str.json");
+    xpath.destroy(t,"['a','b']");
+
+    if(errors.has_errors(t)){
+        char *message =errors.get_error_message(t);
+        int code = errors.get_error_code(t);
+        printf("%d",code);
+        printf("%s",message);
+        xpath.free(t);
+        return 0;
+    }
+    char *result = xpath.dump_to_string(t,true);
+    printf("%s",result);
+    free(result);
+    xpath.free(t);
+
+}
---
+++ exemples/extra/retriving_type.c
@@ -0,0 +1,30 @@
+
+#include "CxpathJson.h"
+
+
+
+CxpathJsonNamespace xpath;
+CxpathJsonErrorNamespace errors;
+
+
+int main(){
+    xpath = newCxpathJsonNamespace();
+    errors = xpath.errors;
+    CxpathJson *t = xpath.new_from_file("tests/target/str.json");
+
+
+    if(errors.has_errors(t)){
+        char *message =errors.get_error_message(t);
+        int code = errors.get_error_code(t);
+        printf("%d",code);
+        printf("%s",message);
+        xpath.free(t);
+        return 0;
+    }
+    printf("exist :%d\n" , xpath.types.exist(t,"['a','b']"));
+    printf("type code:%d\n" , xpath.types.type(t,"['a','b']"));
+    printf("type str:%s\n" , xpath.types.type_str(t,"['a','b']"));
+
+    xpath.free(t);
+
+}
