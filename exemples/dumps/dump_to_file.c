6267405f7a9f4d92386640e07e259dba645d108e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-01 11:34:47 -0300

    att

--- exemples/dumps/dump_to_file.c
+++ exemples/dumps/dump_to_file.c
@@ -9,7 +9,7 @@ int main(){
     xpath = newCxpathJsonNamespace();
     errors = xpath.errors;
     CxpathJson *t = xpath.new_from_file("tests/target/a.json");
-    xpath.dump_to_file(t,"tests/target/b.json", true);
+    xpath.dump_to_file(t,"side_effect/b.json", true);
     xpath.free(t);
     printf("OK");
 
