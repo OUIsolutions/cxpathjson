552fde25e1c7e1147f2346fa8aaeee8765201870
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-01 11:36:59 -0300

    att

--- teste.py
+++
@@ -1,9 +0,0 @@
-
-with open("teste.js",'r') as arq:
-    result = arq.read()
-
-result = result.replace("(",': [')
-result = result.replace(")",']')
-
-with open("result.yaml",'w') as arq:
-    arq.write(result)
