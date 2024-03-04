a103bdcd4b570dded21ef33f155479061777d0a4
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-03 22:42:46 -0300

    at

--- teste2.py
+++
@@ -1,7 +0,0 @@
-
-name = get('Users','Mateus Moutinho','name')
-email = get('Users','Mateus Mootinho','email')
-password = get('Users','Mateus Moutinho','password')
-entity = get('Entity','iassdasasdas')
-docs =list('Entity','asassasasa')
-
